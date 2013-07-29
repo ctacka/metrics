0c04a202d0690f81919b1fbf9fc3376c32f2af38
ctacka <dashkovsky@gmail.com>
2013-07-30 03:45:56 +0400

    a bit moew

--- interactors/__init__.py
+++ interactors/__init__.py
@@ -1,1 +1,19 @@
 __author__ = 'Stas'
+
+
+class CommonInteractor(object):
+    pass
+
+
+class Auth(object):
+    interactors = {'jira': None, 'wiki': None, 'code': None, 'sonar': None}
+
+    def __init__(self, settings=set()):
+        pass
+
+    def add_interactor(self, interactor):
+        pass
+
+    @property
+    def jira(self):
+        return self.interactors.get('jira', None)
